62bfd942bdcee3216e9a5f0f4e1eb09d0862a95b
Lee Kaon <dlrkdhsoff@gmail.com>
2024-05-28 02:05:35 +0900

    backup

---
+++ src/main/java/com/zerobase/wifi/controller/WifiController.java
@@ -0,0 +1,27 @@
+package com.zerobase.wifi.controller;
+
+import java.util.*;
+import com.zerobase.wifi.dto.WifiDTO;
+import com.zerobase.wifi.service.APIService;
+import com.zerobase.wifi.service.WifiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WifiController {
+
+    @Autowired
+    private WifiService wifiService;
+
+    @GetMapping("/wifi-Info")
+    public String wifiInfo(Model model) {
+        List<WifiDTO> list = wifiService.getList();
+        for(WifiDTO wifi : list) {
+            System.out.println(wifi.getMgr_no());
+        };
+        model.addAttribute("list", list);
+        return "home";
+    }
+}
