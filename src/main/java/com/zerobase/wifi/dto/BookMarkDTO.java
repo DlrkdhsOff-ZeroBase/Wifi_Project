88f0d321fc57bf82076eb07b7224257922a264de
Lee Kaon <dlrkdhsoff@gmail.com>
2024-05-30 04:07:20 +0900

    before refactoring

---
+++ src/main/java/com/zerobase/wifi/dto/BookMarkDTO.java
@@ -0,0 +1,11 @@
+package com.zerobase.wifi.dto;
+
+import lombok.Data;
+
+@Data
+public class BookMarkDTO {
+    private long id;
+    private String bookMarkName;
+    private String wifi_name;
+    private String date;
+}
